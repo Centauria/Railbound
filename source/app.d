2805156c4c4d85799c774dc9994c9f8bbb2ce902
Centauria <centauriavc@gmail.com>
2025-06-08 03:12:52 +0800

    basic def

--- source/app.d
+++ source/app.d
@@ -1,5 +1,32 @@
 import std.stdio;
+import std.stdint;
+
+enum Port
+{
+    L,
+    R,
+    U,
+    D
+}
+
+enum CellType{
+    Normal,
+    Locked,
+    Switch,
+    Gate,
+    Tunnel,
+    Platform
+}
+
+struct Cell
+{
+    CellType type;
+    uint32_t ports;
+}
 
 void main()
 {
+    Cell[10] cells;
+    printf("%d\n", cells[2].type);
+    printf("%lld\n", Cell.sizeof);
 }
