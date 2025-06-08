ac7491d7bd1f86aad6d7e329f32c20f5ad621c76
Centauria <centauriavc@gmail.com>
2025-06-09 00:52:01 +0800

    port & connectivity definition

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
 import std.stdio;
 import std.stdint;
+import core.bitop;
 
 enum Port
 {
@@ -9,7 +10,8 @@ enum Port
     D
 }
 
-enum CellType{
+enum CellType
+{
     Normal,
     Locked,
     Switch,
@@ -21,7 +23,22 @@ enum CellType{
 struct Cell
 {
     CellType type;
-    uint32_t ports;
+    uint16_t ports;
+
+    // when nports == 3, connectivity == 0 means the side port connects to lower main port, connectivity == 1 otherwise.
+    // e. g. ports == 0x1110 (LRUD: 0111), connectivity == 0 means R <-> U.
+    // ports == 0x1011 (LRUD: 1101), connectivity == 0 means D <-> L.
+    uint16_t connectivity;
+
+    int nports()
+    {
+        return popcnt(ports);
+    }
+
+    bool isOpen(Port p)
+    {
+        return (ports & (1 << p)) > 0;
+    }
 }
 
 void main()
@@ -29,4 +46,6 @@ void main()
     Cell[10] cells;
     printf("%d\n", cells[2].type);
     printf("%lld\n", Cell.sizeof);
+    cells[2].ports = 0x1110;
+    printf("%d\n", cells[2].nports);
 }
